3c47d69618c3058f5a6ea352c2725786295c64a7
Sarah Usher <sarahnusher@gmail.com>
2024-12-07 17:36:56 +0000

    test four failing

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,10 @@ class Light {
 	constructor() {
 		this.on = false;
 	}
+
+	switchOn(){
+	    this.on = true;
+	}
 }
 
 class Lights {
@@ -11,6 +15,20 @@ class Lights {
 		this.across = across;
 		this.down = down;
 		this.grid = Array(across).fill(Array(down).fill(new Light));
+
+	}
+
+	switchOn(startingPostition, endPosition){
+	    const startX = startingPostition[0]
+	    const startY = startingPostition[1]
+	    const endX = endPosition[0]
+	    const endY = endPosition[1]
+
+	    for (let i = startX; i <= endX; i++){
+	        for (let j = startY; j <= endY; j++){
+	            this.grid[i][j].switchOn()
+	        }
+	    }
 
 	}
 }
--- javascript/index.test.js
+++ javascript/index.test.js
@@ -29,3 +29,17 @@ test('grid of lights starts with lights off', () => {
 		}
 	}
 });
+
+test('switch on specific lights in grid', () => {
+    const lights = new Lights(3, 2);
+
+    lights.switchOn([0,0], [1,1])
+
+    for (let i = 0; i < 2; i++) {
+    		for (let j = 0; j < 2; j++) {
+    			expect(lights.grid[i][j].on).toBe(true);
+    		}
+    	}
+
+    expect(lights.grid[2][1].on).toBe(false);
+});
