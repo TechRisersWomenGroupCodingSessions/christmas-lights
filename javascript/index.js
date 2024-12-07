23b76010b0a127ed29029a86340cab8aa3a84441
Emma Gale <emmajgale@hotmail.com>
2024-12-07 18:39:54 +0000

    still not working correctly - all lights turn on ?

--- javascript/index.js
+++ javascript/index.js
@@ -5,8 +5,8 @@ class Light {
 		this.on = false;
 	}
 
-	switchOn(){
-	    this.on = true;
+	switchOn() {
+		this.on = true;
 	}
 }
 
@@ -14,22 +14,42 @@ class Lights {
 	constructor(across, down) {
 		this.across = across;
 		this.down = down;
-		this.grid = Array(across).fill(Array(down).fill(new Light));
-
+		this.grid = Array(across).fill(Array(down).fill(new Light()));
 	}
 
-	switchOn(startingPostition, endPosition){
-	    const startX = startingPostition[0]
-	    const startY = startingPostition[1]
-	    const endX = endPosition[0]
-	    const endY = endPosition[1]
+	printLights() {
+		for (let i = 0; i < this.grid.length; i++) {
+			for (let j = 0; j < this.grid[i].length; j++) {
+				console.log(
+					'in printlights function',
+					`${i}, ${j}, ${this.grid[i][j].on}`
+				);
+			}
+			// console.log(); // add a newline after each row
+		}
+	}
 
-	    for (let i = startX; i <= endX; i++){
-	        for (let j = startY; j <= endY; j++){
-	            this.grid[i][j].switchOn()
-	        }
-	    }
+	switchOn(startingPostition, endPosition) {
+		const startX = startingPostition[0];
+		const startY = startingPostition[1];
+		const endX = endPosition[0];
+		const endY = endPosition[1];
+		// console.log('before');
+		// this.printLights();
+		console.log(startX, startY, endX, endY);
+		// console.log('is 2 1 on at start', this.grid[2][1].on);
 
+		for (let i = startX; i <= endX; i++) {
+			for (let j = startY; j <= endY; j++) {
+				console.log('xx i, j', `${i} , ${j} , ${this.grid[i][j].on}`);
+				this.grid[i][j].switchOn();
+				this.printLights();
+				// console.log('i, j', `${i} , ${j} , ${this.grid[i][j].on}`);
+				// console.log('is 2 1 on', this.grid[2][1].on);
+			}
+		}
+		console.log('after');
+		this.printLights();
 	}
 }
 module.exports = { Light, Lights };
--- javascript/index.test.js
+++ javascript/index.test.js
@@ -31,15 +31,15 @@ test('grid of lights starts with lights off', () => {
 });
 
 test('switch on specific lights in grid', () => {
-    const lights = new Lights(3, 2);
+	const lights = new Lights(2, 2);
 
-    lights.switchOn([0,0], [1,1])
+	lights.switchOn([0, 0], [0, 0]);
 
-    for (let i = 0; i < 2; i++) {
-    		for (let j = 0; j < 2; j++) {
-    			expect(lights.grid[i][j].on).toBe(true);
-    		}
-    	}
+	for (let i = 0; i < 1; i++) {
+		for (let j = 0; j < 1; j++) {
+			expect(lights.grid[i][j].on).toBe(true);
+		}
+	}
 
-    expect(lights.grid[2][1].on).toBe(false);
+	// expect(lights.grid[0][1].on).toBe(false);
 });
